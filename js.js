c5b9f6fbadc0024acab2c27a79383b9a430d1ab1
Vins-novaldi <vijjaynovaldi01@gmail.com>
2025-05-30 23:43:04 +0700

    edit

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 
-  // Simpan preferensi pengguna di localStorage
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
   } else {
@@ -9,10 +8,26 @@ function toggleDarkMode() {
   }
 }
 
-// Cek preferensi pengguna saat halaman dimuat
 window.onload = function() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
   }
 }
+
+// //////////////////////////////////////
+
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-100px";
+  } else {
+    navbar.style.top = "0";
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
